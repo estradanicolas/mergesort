6cfcce9c5123d891069dd0ed7bf44a703aa47744
Nicolas Estrada <59927633+estradanicolas@users.noreply.github.com>
2020-05-10 12:57:01 -0500

    Add files via upload

---
+++ build/precache-manifest.780c2ad4c66230b91ffff371487b5044.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8259b9d99a01f31400b3af7c70a8df6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e635b08b313fdd51d16b",
+    "url": "/static/css/main.e8547d95.chunk.css"
+  },
+  {
+    "revision": "ae4e3fa89f350a97ec16",
+    "url": "/static/js/2.6d8871a7.chunk.js"
+  },
+  {
+    "revision": "e635b08b313fdd51d16b",
+    "url": "/static/js/main.cb6f388f.chunk.js"
+  },
+  {
+    "revision": "f532322b6dd41c06a732",
+    "url": "/static/js/runtime-main.7c419107.js"
+  }
+]);
